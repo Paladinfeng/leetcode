e56c5e17c532230131cd471ae309dbe206d66ff4
xuezhaofeng <zfxue@icloud.com>
2016-10-15 11:16:34 +0800

    feat: 初始化项目

---
+++ leetcode/CountingBits/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  CountingBits
+//
+//  Created by xuezhaofeng on 2016/10/14.
+//  Copyright © 2016年 paladinfeng. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+func countBits(_ num: Int) -> [Int] {
+    
+}
---
+++ leetcode/leetcode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  leetcode
+//
+//  Created by xuezhaofeng on 2016/10/14.
+//  Copyright © 2016年 paladinfeng. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
